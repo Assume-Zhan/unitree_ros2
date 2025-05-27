8c913262dac030e84b6c294843011a7cf538828a
assume <assume0701@gmail.com>
2025-05-27 21:01:38 +0800

    feat(cmd_vel_converter): Update the code and naming style

--- example/src/src/cmd_vel_convert.cpp
+++ example/src/src/cmd_vel_convert.cpp
@@ -1,4 +1,3 @@
-
 #include <unistd.h>
 #include <cmath>
 
@@ -11,30 +10,29 @@
 #include "geometry_msgs/msg/twist.hpp"
 
 using std::placeholders::_1;
-// Create a soprt_request class for soprt commond request
-class soprt_request : public rclcpp::Node
-{
+
+class cmd_vel_converter : public rclcpp::Node {
+
 public:
-    soprt_request() : Node("req_sender")
-    {
+    cmd_vel_converter() : Node("cmd_vel_converter") {
+
         cmd_vel_suber = this->create_subscription<geometry_msgs::msg::Twist>(
-            "cmd_vel", 10, std::bind(&soprt_request::cmd_vel_callback, this, _1));
+            "cmd_vel", 10, std::bind(&cmd_vel_converter::cmd_vel_callback, this, _1));
 
-        // the req_puber is set to subscribe "/api/sport/request" topic with dt
         req_puber = this->create_publisher<unitree_api::msg::Request>("/api/sport/request", 10);
-
-        // t = -1; // Runing time count
     };
 
 private:
 
-    void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
-    {
+    void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg) {
+
         float vx = msg->linear.x;
         float vy = msg->linear.y;
         float vyaw = msg->angular.z;
 
-        sport_req.Move(req, vx, vy, vyaw); // Set the request to move
+        // Set the request to move
+        sport_req.Move(req, vx, vy, vyaw);
+
         // Publish the request
         req_puber->publish(req);
     }
@@ -44,17 +42,22 @@ private:
 
     rclcpp::Publisher<unitree_api::msg::Request>::SharedPtr req_puber;
 
-    unitree_api::msg::Request req; // Unitree Go2 ROS2 request message
+    // Unitree Go2 ROS2 request message
+    unitree_api::msg::Request req;
     SportClient sport_req;
 };
 
-int main(int argc, char *argv[])
-{
-    rclcpp::init(argc, argv); // Initialize rclcpp
-    rclcpp::TimerBase::SharedPtr timer_; // Create a timer callback object to send sport request in time intervals
+int main(int argc, char *argv[]) {
 
-    rclcpp::spin(std::make_shared<soprt_request>()); //Run ROS2 node
+    // Initialize rclcpp
+    rclcpp::init(argc, argv);
+
+    // Create a timer callback object to send sport request in time intervals
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    rclcpp::spin(std::make_shared<cmd_vel_converter>());
 
     rclcpp::shutdown();
+
     return 0;
 }
