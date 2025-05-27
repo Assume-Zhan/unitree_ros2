3d207db253a9bb4fd427f2a07bfb0b8b3c3c7ff6
assume <assume0701@gmail.com>
2025-05-27 20:54:07 +0800

    feat(cyclonedds): Update cyclonedds settings

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 echo "Setup unitree ros2 environment"
-source /opt/ros/foxy/setup.bash
-source $HOME/unitree_ros2/cyclonedds_ws/install/setup.bash
+source /opt/ros/humble/setup.bash
+source $HOME/workspace/cyclonedds_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
-                            <NetworkInterface name="enp3s0" priority="default" multicast="default" />
+                            <NetworkInterface name="enp4s0" priority="100" multicast="default" />
                         </Interfaces></General></Domain></CycloneDDS>'
+
+# # Debug for CyCloneDDS settings
+# export CYCLONEDDS_URI='<CycloneDDS><Domain>
+#                          <General><Interfaces>
+#                            <NetworkInterface name="enp4s0" priority="3" multicast="default" />
+#                          </Interfaces></General>
+#                          <Tracing>
+#                            <Verbosity>finest</Verbosity>
+#                          </Tracing>
+#                        </Domain></CycloneDDS>'
